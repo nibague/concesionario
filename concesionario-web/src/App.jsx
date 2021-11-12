491d4603a36562fdf7cf67e9ab8e09dd3299b340
nibague <nikolasibagues@gmail.com>
2021-11-12 11:00:07 -0500

    agregando paginas y rutas

--- concesionario-web/src/App.jsx
+++ concesionario-web/src/App.jsx
@@ -1,10 +1,25 @@
 import 'styles/styles.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/login'>
+          <Login/>
+        </Route>
+        <Route path='/registro'>
+          <Registro/>
+        </Route>
+        <Route path='/admin'>
+          <Admin/>
+        </Route>
+        <Route path='/'>
+          <Index/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ concesionario-web/src/pages/Registro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registro = () => {
+    return (
+        <div>
+            website to register 
+        </div>
+    )
+}
+
+export default Registro
