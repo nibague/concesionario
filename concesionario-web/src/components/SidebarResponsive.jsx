5c5b7c649626c4404e07eb7000f824763588bc17
nibague <nikolasibagues@gmail.com>
2021-11-29 16:24:53 -0500

    agregando propiedades responsive en tailwindcss

--- concesionario-web/src/components/SidebarResponsive.jsx
+++ concesionario-web/src/components/SidebarResponsive.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const SidebarResponsive = () => {
+    const [mostrarNavegacion, setMostrarNavegacion] = useState(false);
+    let solids = 'solid()';
+
     return (
-        <div className='md:hidden'>
-            <FontAwesomeIcon className='hover:text-yellow-600' icon={solid('bars')} />
+        <div className='md:hidden' 
+        onClick={() => {
+            setMostrarNavegacion(!mostrarNavegacion)
+        }}>
+            <FontAwesomeIcon className='hover:text-yellow-600' icon={mostrarNavegacion ? solid('times') : solid('bars')} />
         </div>
     )
 }
