6b84833c0799f91465a2f14a4059b412354ef800
nibague <nikolasibagues@gmail.com>
2021-11-29 14:14:56 -0500

    agregando medida de sidebar

--- concesionario-web/src/components/Sidebar.jsx
+++ concesionario-web/src/components/Sidebar.jsx
@@ -9,12 +9,12 @@ import { fas, fa } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
     return (
-        <nav className='border bg-gray-300 h-full flex flex-col p-4 sidebar'>
+        <nav className='border bg-gray-300 h-full flex flex-col p-4 sidebar w-80'>
             <Link to='/admin'>
                 <ImagenLogo />
             </Link>
             <div className='my-4'>
-                {/* en el componente ruta se especifican los parametros (icono) donde se escribe categoria y nombre del icono de fontawesome */}
+                {/* en el componente "Ruta" se especifican los parametros (icono) donde se escribe categoria y nombre del icono de fontawesome */}
                 <Ruta icono={solid('user')} ruta='/admin/perfil' nombre='perfil'/>
                 <Ruta icono={solid('car')} ruta='/admin/vehiculos' nombre='vehiculos'/>
                 <Ruta icono={solid('cash-register')} ruta='/admin/ventas' nombre='ventas'/>
@@ -28,7 +28,7 @@ const Sidebar = () => {
 const Ruta = ({icono, ruta, nombre}) => {
     return (
         <Link to={ruta}>
-            <button className='my-1 text-lg pl-7 p-2 bg-indigo-400 hover:bg-indigo-800 flex items-center w-full text-white rounded-md'>
+            <button className='my-1 text-lg pl-7 p-2 bg-indigo-400 hover:bg-indigo-800 flex items-center text-white rounded-md w-full'>
                 {/* <i className={`${icono} w-10`}/> el profesor puso este tag para llamar a los iconos de fontawesome pero no funcionan si no se instalan las dependencias y el componente */}
                 <FontAwesomeIcon className='mr-5' icon={icono} />
                 {nombre}
