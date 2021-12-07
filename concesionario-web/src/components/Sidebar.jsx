c9473d6c22af0b5e005144e76dcc57775b640abb
nibague <nikolasibagues@gmail.com>
2021-12-06 19:57:39 -0500

    agregando useEffect para realtar boton del sidebar cuando esta seleccionado

--- concesionario-web/src/components/Sidebar.jsx
+++ concesionario-web/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ImagenLogo from './ImagenLogo';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { useLocation } from 'react-router-dom';
 
 
 const Sidebar = () => {
@@ -25,9 +26,21 @@ const Sidebar = () => {
 };
 
 const Ruta = ({icono, ruta, nombre}) => {
+    const location = useLocation();
+    const [isActive, setIsActive] = useState(false);
+    useEffect(() => {
+        console.log(location, ruta);
+        if(location.pathname.includes(ruta)){ //esto permite que el boton del sidebar actual se resalte aun asi este en otra ruta dentro de la actual
+            setIsActive(true);
+        } else{
+            setIsActive(false);
+        }
+    }, [location, ruta])
     return (
         <Link to={ruta}>
-            <button className='my-1 text-lg pl-7 p-2 bg-indigo-400 hover:bg-indigo-800 flex items-center w-full text-white rounded-md'>
+            <button className={`my-1 text-lg pl-7 p-2 bg-${
+                isActive ? 'indigo' : 'gray'
+            }-700 hover:bg-indigo-900 flex items-center w-full text-white rounded-md`}>
                 {/* <i className={`${icono} w-10`}/> el profesor puso este tag para llamar a los iconos de fontawesome pero no funcionan si no se instalan las dependencias y el componente */}
                 <FontAwesomeIcon className='mr-5' icon={icono} />
                 {nombre}
---
+++ concesionario-web/src/hooks/useActiveRoute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useActiveRoute = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default useActiveRoute
